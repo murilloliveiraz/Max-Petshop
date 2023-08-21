9dbd97b65aff0e9798885625c143c778e8b74c6b
Murillo Oliveira <murillo1047@gmail.com>
2023-08-21 11:01:32 -0300

    json fetch

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -13,15 +13,35 @@ const criarNovaLinha = (nome, email) => {
 
 const tabela = document.querySelector('[data-tabela]');
 
-const http = new XMLHttpRequest();
-
-http.open('GET', 'http://localhost:3000/profile');
-
-http.send();
-
-http.onload= () => {
-    const data = JSON.parse(http.response);
-    data.forEach(element => {
-        tabela.appendChild(criarNovaLinha(element.nome,element.email));
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
+    .then(resposta => {
+        return resposta.json();
     })
+
 }
+
+listaClientes()
+.then(
+    data => {
+        data.forEach(element => {
+            tabela.appendChild(criarNovaLinha(element.nome,element.email));
+        });
+})
+
+// const promise = new Promise((resolve, reject) => {
+    //     const http = new XMLHttpRequest();
+    
+    //     http.open('GET', 'http://localhost:3000/profile');
+    
+    //     http.onload = () => {
+    //         if(http.status >= 400){
+    //             reject(JSON.parse(http.response));
+    //         } else {
+    //             resolve(JSON.parse(http.response));
+     //         }
+     //     }
+    //   http.send();
+     // })
+    // console.log(promise);
+    // return promise;
