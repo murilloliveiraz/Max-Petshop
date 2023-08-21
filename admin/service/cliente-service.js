0235a1abfef799fb2abb07cfeb15b0c21fae30c5
Murillo Oliveira <murillo1047@gmail.com>
2023-08-21 11:07:49 -0300

    json fetch

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,18 +1,3 @@
-const criarNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr');
-    const conteudo = ` <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td><ul class="tabela__botoes-controle">
-        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-    </ul>
-</td>`
-    linhaNovoCliente.innerHTML = conteudo;
-    return linhaNovoCliente;
-}
-
-const tabela = document.querySelector('[data-tabela]');
-
 const listaClientes = () => {
     return fetch(`http://localhost:3000/profile`)
     .then(resposta => {
